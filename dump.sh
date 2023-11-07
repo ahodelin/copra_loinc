4eefd2899335326eed0604bf412258a6394ac8ad
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-11-07 15:42:46 +0100

    Last update Di 7. Nov 15:42:46 CET 2023

--- dump.sh
+++ dump.sh
@@ -1,1 +1,1 @@
-pg_dump -U her11a_admin -d staging_test --schema=loinc_copra > /home/ahodelin/git_repos/copra_loinc/sql/dump/dump_copra_loinc.sql
+/usr/local/pg16/bin/pg_dump -U her11a_admin -d staging_test --schema=loinc_copra > /home/ahodelin/git_repos/copra_loinc/sql/dump/dump_copra_loinc.sql
